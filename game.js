17fbfe51cdbdcc644470e9e79b7c8fa73f0e3ac3
riccdigital <eryc.mabonga@gmail.com>
2021-01-14 16:26:25 +0300

    end game update

--- game.js
+++ game.js
@@ -1,7 +1,11 @@
 
 // Game Variables
-const guess = 50
+const guess = 1 + Math.floor(Math.random() * 50)
 let playerGuess = 0
+let guessesRemaining = 10
+let guessesMade = 0
+let gameState = ''
+let gameWon = false
 
 // Input & Output
 const input = document.getElementById('input')
@@ -16,13 +20,31 @@ function clickHandler () {
 }
 
 function playGame () {
+  guessesRemaining = guessesRemaining - 1
+  guessesMade = guessesMade + 1
+  gameState = 'Guess: ' + guessesMade + ', Remaining: ' + guessesRemaining
   playerGuess = Number(input.value)
 
   if (playerGuess > guess) {
-    output.innerHTML = 'That is too high!'
+    output.innerHTML = 'That is too high! ' + gameState
+    if (guessesRemaining < 1) {
+      endGame()
+    }
   } else if (playerGuess < guess) {
-    output.innerHTML = 'That is too low!'
+    output.innerHTML = 'That is too low! ' + gameState
+    if (guessesRemaining < 1) {
+      endGame()
+    }
   } else if (playerGuess === guess) {
-    output.innerHTML = 'Got It!!!'
+    gameWon = true
+    endGame()
+  }
+}
+
+function endGame () {
+  if (gameWon) {
+    output.innerHTML = "Yes, it's" + guess + '!' + '<br>' + 'It only took you' + guessesMade + ' guesses.'
+  } else {
+    output.innerHTML = 'No more guesses left!' + '<br>' + 'The mystery number was: ' + guess + '.'
   }
 }
