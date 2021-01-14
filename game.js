072a5044fa1338edb1efac1c5d6a4f61a4d249e3
riccdigital <eryc.mabonga@gmail.com>
2021-01-14 15:29:55 +0300

    first functional code

---
+++ game.js
@@ -0,0 +1,28 @@
+
+// Game Variables
+const guess = 50
+let playerGuess = 0
+
+// Input & Output
+const input = document.getElementById('input')
+const output = document.getElementById('output')
+
+const button = document.querySelector('button')
+button.style.cursor = 'pointer'
+button.addEventListener('click', clickHandler, false)
+
+function clickHandler () {
+  playGame()
+}
+
+function playGame () {
+  playerGuess = parseInt(input.value)
+
+  if (playerGuess > guess) {
+    output.innerHTML = 'That is too high!'
+  } else if (playerGuess < guess) {
+    output.innerHTML = 'That is too low!'
+  } else if (playerGuess === guess) {
+    output.innerHTML = 'Got It!!!'
+  }
+}
